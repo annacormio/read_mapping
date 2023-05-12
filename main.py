9fb67ddc3ac51572d321104c1fba30162d2cc5cc
Anna <annacormio01@gmail.com>
2023-05-12 19:07:51 +0200

    bwt

--- main.py
+++ main.py
@@ -7,22 +7,43 @@ def getGenome(length=1000):
     genome = "".join(random.choice('ATCG') for i in range(length))
     return genome
 
+
+
+def sortedRotations(t):
+    s=t
+    rot=[s]
+    s=s[1:]+s[0]
+    while s!=t:
+        rot.append(s)
+        s = s[1:] + s[0]
+    return sorted(rot)
+
+
 def bwt(t): #implement Burrows-Wheeler transform compression of the string
-    s=t.copy()
-    s.remove(t[0])
-    s.append(t[0])
-
-
-    rot=[]
-
-
+    r=sortedRotations(t)
+    bwtstring=''
+    for s in r:
+        bwtstring+=s[-1:]
+    return bwtstring
 
 
 
-def revBwt():
+
+
+
+
+
+
+
+
+#def revBwt():
 
 
 
 
 if __name__=="__main__":
-    t=getGenome(20)
+    #t=getGenome(5)
+    t='ATCG$'
+    print(t)
+    print(sortedRotations(t))
+    print(bwt(t))

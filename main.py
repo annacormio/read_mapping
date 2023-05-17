5826675b5fc5fbdb890ba0c96d89b6596da1b36d
Anna <annacormio01@gmail.com>
2023-05-17 21:34:28 +0200

    g

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import itertools
 
 
+
 # INPUT
 def getGenome(length=1000):
     genome = "".join(random.choice('ATCG') for i in range(length))
@@ -16,7 +17,10 @@ def sortedRotations(t):
     while s!=t:
         rot.append(s)
         s = s[1:] + s[0]
-    return sorted(rot)
+
+    rot = sorted(rot)
+    print("Sorted Rotations: ", rot)
+    return rot
 
 
 def bwt(t): #implement Burrows-Wheeler transform compression of the string
@@ -27,23 +31,50 @@ def bwt(t): #implement Burrows-Wheeler transform compression of the string
     return bwtstring
 
 
+def revBwt(b):
+    t=""
+    #Counts each char occurrencies
+    chars = list(b)  #corresponds to last matrix column
+    ordered = sorted(chars) #corresponds to the first matrix column
+    print(chars, "\n", ordered)
+    inverted = ""
+    c = ordered[0]
+    i = 0
+    count = 0
+    countOccorrenze = 0
+    countNuoveOccorrenze = 0
+    for i in range(len)
+
+
+        countOccorrenze = 0
+        countNuoveOccorrenze = 0
+        count += 1
+
+    while True:
+        inverted += c
+        c = chars[i]
+        occ = chars[0:i + 1].count(c)
+
+        idxs = [i for i, x in enumerate(ordered) if x == c] #given a list and an element, find all the occurrencies of that element in the list
+        i = idxs[occ-1]
 
 
 
 
-
-
-
-
-
-#def revBwt():
-
-
+    original = inverted[::-1]
+    return original
 
 
 if __name__=="__main__":
     #t=getGenome(5)
-    t='ATCG$'
-    print(t)
-    print(sortedRotations(t))
-    print(bwt(t))
+    t='AAACTTG$'
+    print("Orig.: ", t)
+
+    transformed = bwt(t)
+    print("Transformed: ", transformed)
+
+    original = revBwt(transformed)
+    print(original)
+
+
+

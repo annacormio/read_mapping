a582a2ebfb789ca8eee403de1cf89af4b6966d68
Anna <annacormio01@gmail.com>
2023-06-07 10:23:59 +0200

    match start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 # INPUT
 def getGenome(length=1000): #generate a random genome seq ending with $ character
     genome = "".join(random.choice('ATCG') for i in range(length))
@@ -34,10 +35,24 @@ def all_indexes(l, c):
     return idxs
 
 
-#finds the number of occurrences of an element c in the list l searching it up to the indexed position i
-def occurrences(l,i,c):
+#returns the rank of a given character c in position i in a given list l
+def rank(c,l,i):
     return l[0: (i + 1)].count(c) #in the sliced list from 0 to the index i+1 of c (c included) in the list l, counts how many c characters occur
 
+
+#returns the index of a character c in a list l known its rank r
+def position(c,l,r):
+    count=-1 #count of the rank encountered
+    pos=-1 #store position
+    while count<r and pos<=(len(l)-1):
+        pos += 1
+        n=l[pos]
+        if n==c:
+            count+=1
+    return pos
+
+
+#compute reverse transformation of bwt
 def revBwt(b):
     L= list(b) #last line of the bwt matrix is the bwt
     F = sorted(b) #the first line of the bwt matrix is  basically the bwt sorted
@@ -46,22 +61,82 @@ def revBwt(b):
     inverted = "$" #intantiate the reverse transformation string of bwt
     while(c != '$'): #until we find in L a $
         inverted += c
-        occ_c_L = occurrences(L,index,c) #counting number of occurences of c in L up to the position of L in which we are
-        index = all_indexes(F,c)[occ_c_L-1]
+        occ_c_L = rank(c, L, index)  #counting number of occurences of c in L up to the position of L in which we are
+        index = position(c,F,occ_c_L-1)
         c = L[index]
     original = inverted[::-1] #we recunstructed from the bottom of the string so we need to invert it to have the original
     return original
 
+#defines if p read maps on t
+def match(t, p):
+    F = list(sorted(t))  # first string
+    L = list(bwt(t))  # bwt string
+    q = list(p)[:-1] #all characters of p are elements of a list deleting the dollar
+    sol=[] #initialize solution list indx in L, more than one solution is possible
+    c = q.pop(-1) #last query character
+    
+    #operations for first character check, check all in F list at the beginning
+    indx = all_indexes(F, c)  # list of indexes of occurence of c in F
+    for i in indx:
+        if L[i] == q[-1]:  # as soon as I find the next character in L i break
+            sol.append(i) #add the index to the list
+            
+    #further operations on the rest of the character
+    while len(q)>1 and len(sol)>0:
+        pos = sol.copy()  # create a copy of sol
+        sol.clear()
+        c=q.pop(-1)
+        for s in pos:
+            r = rank(c,L,s)
+            p=position(c,F,r)
+            if L[p]==q[-1]:
+                sol.append(p)
+        
+    return sol
+        
+        
+    
+
+
+
+
+#def offset(t):
+
+
+#def match(t,p):
+
+
+
+
+
+
+
 if __name__=="__main__":
-    t=getGenome(10)
-    #t = 'ACACAGTACCGTA$'
+    #t=getGenome(20)
+    t = 'ACACAGTACCGTACATTTCAGTAACACAGTCA$'
+    p='GTA'
     print("Original: ", t)
+    print('Query: ',p)
     rotations=sortedRotations(t)
-    print("soted rotations",rotations)
-    transformed = bwt(t)
-    print("Transformed: ", transformed)
-    original = revBwt(transformed)
-    print("Reversed bwt: ",original)
+    #print("soted rotations",rotations)
+    L = bwt(t)
+    F = sorted(t)
+    print("Transformed: ", L)
+    original = revBwt(L)
+    #print("Reversed bwt: ",original)
+    match=match(t,p)
+    print('matching? ',match)
+    print('position',position('T',list(t),4))
+
+
+
+
+
+
+
+
+
+
 
 
 
